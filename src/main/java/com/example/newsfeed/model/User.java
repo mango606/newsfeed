4430a50a0ab5495e7e6097703aae7bf72d3c6758
mango606 <yuj08184@kangwon.ac.kr>
2025-10-19 14:29:44 +0900

    feat: 사용자 엔티티 및 팔로우 관계 구현

---
+++ src/main/java/com/example/newsfeed/model/User.java
@@ -0,0 +1,52 @@
+package com.example.newsfeed.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String bio;
+
+    private String profileImage;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    // 팔로우 관계
+    @ManyToMany
+    @JoinTable(
+            name = "user_follows",
+            joinColumns = @JoinColumn(name = "follower_id"),
+            inverseJoinColumns = @JoinColumn(name = "following_id")
+    )
+    @Builder.Default
+    private Set<User> following = new HashSet<>();
+
+    @ManyToMany(mappedBy = "following")
+    @Builder.Default
+    private Set<User> followers = new HashSet<>();
+}
