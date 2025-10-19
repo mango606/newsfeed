07e5fa66e07cfa567681aa6f123feb2da6088c4b
mango606 <yuj08184@kangwon.ac.kr>
2025-10-19 13:23:11 +0900

    Initial commit

---
+++ src/test/java/com/investory/newsfeed/NewsfeedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investory.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
